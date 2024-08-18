f48699c94d0f8fd4478f89832773c1dd0c87fcbe
Wendie <wendymathabela@gmail.com>
2024-08-18 19:09:30 +0200

    modified footer

--- src/Footer.js
+++ src/Footer.js
@@ -10,7 +10,23 @@ export default function Footer() {
         rel="noreferrer"
       >
         {" "}
-        Wendy Mathabela.
+        Wendy Mathabela and is
+        <a
+          href="https://github.com/wendiemat/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          on GitHub
+        </a>{" "}
+        and
+        <a
+          href="https://glittery-biscuit-9bee29.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
       </a>
     </div>
   );

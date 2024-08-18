8c8d607ebe39da9abfa9ea96b330c873bd72b8c9
Wendie <wendymathabela@gmail.com>
2024-08-18 17:54:57 +0200

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello <code>React</code>
+        </h1>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
